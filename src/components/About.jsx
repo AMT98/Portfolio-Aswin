a33f4fca639b5dcae16255259a3d10f6470c0976
AMT98 <aswinmalla12@gmail.com>
2023-02-05 10:16:00 -0700

    Update font sizes for about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.png";
 
-
 const About = () => {
   return (
     <div name="about" className="w-full h-screen bg-[#0A192F] text-[#838DAB]">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="flex flex-col justify-center h-full">
-          <div className="pb-8" data-aos= "fade-right">
-            <p className="text-4xl font-bold inline border-b-4 border-gray-500 text-[#00CCAF]" >
+          <div className="pb-8" data-aos="fade-right">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500 text-[#00CCAF]">
               About
             </p>
           </div>
           <div className="flex flex-col sm:flex-row">
             <div data-aos="fade-right">
-              <p className="text-white-500 font-bold md:text-md text-sm py-4 max-w-md">
+              <p className="text-white-500 font-bold md:text-base text-xs py-4 max-w-md">
                 I am a full-stack developer with over a year of experience
                 working with the PERN stack (PostgreSQL, Express, React, and
                 Node.js). I have a passion for creating efficient and visually
@@ -22,7 +21,7 @@ const About = () => {
                 knowledge and stay up to date with the latest web development
                 technologies.
               </p>
-              <p className="text-white-500 font-bold md:text-md text-sm py-4 max-w-md">
+              <p className="text-white-500 font-bold md:text-base text-xs py-4 max-w-md">
                 I have experience in developing responsive front-end using React
                 and TailwindCSS and a solid understanding of HTML, CSS, and
                 JavaScript. On the back-end, I have experience with Node.js,
@@ -35,11 +34,11 @@ const About = () => {
                 out, cooking, and spending time with my family.
               </p>
             </div>
-            <div className="w-5/12 " data-aos= "fade-left">
+            <div className="w-7/12 " data-aos="fade-left">
               <img
                 src={HeroImage}
                 alt="profilepic"
-                className="shadow-xl rounded-full max-w-full h-auto align-middle opacity-50 hover:opacity-100 hover:scale-95"
+                className="shadow-xl rounded-full max-w-full h-auto align-middle"
               />
             </div>
           </div>
