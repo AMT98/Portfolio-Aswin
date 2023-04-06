8afd2ed0ad3fd7551b14b62c60e5707ab345770a
AMT98 <aswinmalla12@gmail.com>
2023-04-06 10:20:28 -0600

    Added blur to bg onclick of the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import { MdInfoOutline } from "react-icons/md";
 
-const Modal = (
-  {
-    modalTxt,
-    modalDescription
-  }
-) => {
+const Modal = ({ modalTxt, modalDescription }) => {
   const [showModal, setShowModal] = useState(false);
   return (
     <>
@@ -19,16 +14,16 @@ const Modal = (
       </button>
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto- fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="justify-center items-center flex overflow-x-hidden backdrop-blur-[5px] overflow-y-auto- fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative m-auto max-w-md">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-xs text-black font-semibold">{modalTxt}</h3>
+                  <h3 className="text-xs text-black font-semibold">
+                    {modalTxt}
+                  </h3>
                 </div>
                 <div className="relative p-4 flex-auto">
-                  <p className="text-xs  text-black">
-                    {modalDescription}
-                  </p>
+                  <p className="text-xs  text-black">{modalDescription}</p>
                 </div>
                 <div className="flex items-center justify-end  border-t border-solid border-slate-200 rounded-b">
                   <button
